1c3f4f306c31bf541cfd1118f7a50f428a1e3611
SyberiaK <syberiakey@gmail.com>
2023-08-16 14:35:32 +0500

    Extracted user_gamestats_text HTML markup info separate file

--- functions/info_formatters.py
+++ functions/info_formatters.py
@@ -1,15 +1,22 @@
 import datetime as dt
+from pathlib import Path
+import re
 from zoneinfo import ZoneInfo
 
 from babel.dates import format_datetime
+from jinja2 import Environment, FileSystemLoader
 
 from .locale import locale
+from l10n import Locale
 from utypes import States
 
 
 VALVE_TIMEZONE = ZoneInfo('America/Los_Angeles')
 CLOCKS = ('🕛', '🕐', '🕑', '🕒', '🕓', '🕔',
           '🕕', '🕖', '🕗', '🕘', '🕙', '🕚')
+
+env = Environment(loader=FileSystemLoader(Path(__file__).parent.parent))
+game_stats_template = env.get_template('game_stats_template.html')
 
 
 def format_server_status(data, lang_code: str):
@@ -91,3 +98,16 @@ def format_valve_hq_time(lang_code: str):
                             f'({valve_hq_datetime:%Z})'
 
     return loc.valve_hqtime_text.format(CLOCKS[valve_hq_datetime.hour % 12], valve_hq_dt_formatted)
+
+
+def format_user_game_stats(stats, _locale: Locale):
+    rendered_page = game_stats_template.render(**_locale.to_dict())
+
+    # for some reason telegraph interprets newline <li></li> as two <li></li>, one of which is empty
+    rendered_page = re.sub(r'\s*<li>\s*', '<li>', rendered_page)  # remove spaces before and after <li>
+    rendered_page = re.sub(r'\s*</li>\s*', '</li>', rendered_page)  # remove spaces before and after </li>
+
+    # with open(Path(__file__).parent.parent / 'rendered_game_stats.html', 'w', encoding='utf8') as file:
+    #     file.write(rendered_page)
+
+    return rendered_page.format(*stats)
