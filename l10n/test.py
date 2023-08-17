376d956d60b883542b26fbae9d01414ee64b2d79
SyberiaK <syberiakey@gmail.com>
2023-08-17 16:33:07 +0500

    Less messy output from L10n test

--- l10n/test.py
+++ l10n/test.py
@@ -5,8 +5,9 @@ def test_l10n(recwarn):
     """
     Test to check any missing or unexpected locale keys.
     """
+
     L10n('data')
 
     for r in recwarn:
         print(f'{r.category.__name__}: {r.message}')
-    assert len(recwarn) == 0, f'Warnings raised: {[f"{r.category.__name__}: {r.message}" for r in recwarn]}'
+    assert len(recwarn) == 0, 'L10n process raised some warnings.'
