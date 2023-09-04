4241eeb4de2f139907f6ca6a3eb4db058c0bcefc
SyberiaK <syberiakey@gmail.com>
2023-09-04 20:16:45 +0500

    Some fixes to locale applying

--- functions/locale.py
+++ functions/locale.py
@@ -1,13 +1,15 @@
+from babel import Locale as BabelLocale, UnknownLocaleError
+
 from l10n import Locale, locale as _loc, get_available_languages as _gal
 
 
-CIS_LANG_CODES = ('be', 'kk')
+CIS_LANG_CODES = ('kk',)
 
 
 def locale(lang: str = 'en') -> Locale:
     """Returns a Locale object based of user's language."""
 
-    if lang in CIS_LANG_CODES:
+    if lang in CIS_LANG_CODES and lang not in get_available_languages():
         lang = 'ru'
 
     return _loc(lang)
@@ -15,8 +17,14 @@ def locale(lang: str = 'en') -> Locale:
 
 def get_refined_lang_code(_locale: Locale) -> str:
     """Get refined lang code that Babel can accept."""
+    lang_code = _locale.lang_code.replace('-', '_')
 
-    return _locale.lang_code.replace('-', '_')
+    try:
+        BabelLocale.parse(lang_code)
+    except UnknownLocaleError:
+        lang_code = 'en'
+
+    return lang_code
 
 
 def get_available_languages() -> dict[str, str]:
