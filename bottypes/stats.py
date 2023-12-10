a860318c13a49fb1fb64cdaf5eca5db5590f4d1a
SyberiaK <syberiakey@gmail.com>
2023-12-10 21:40:15 +0500

    Made Statistic generic, StatisticList

--- bottypes/stats.py
+++ bottypes/stats.py
@@ -1,27 +1,57 @@
+from abc import ABC, abstractmethod
 from dataclasses import dataclass, fields
 
 
-class StatisticInt:  # for now just int descriptor
-    value = 0        # maybe make a generic one?
+class Statistic(ABC):  # yes, I'm a ~~crazy idiot~~ silly cat
+    _value = None
+
+    def __init__(self):
+        if self._value is None:
+            raise ValueError(f'initial value (_value) for {self.__class__.__name__} not assigned')
+        self._type = type(self._value)
 
     def __get__(self, instance, owner=None):
-        return self.value
+        return self._value
 
     def __set__(self, instance, value):
-        self.value = value
+        if not isinstance(value, self._type):
+            raise TypeError(f'Wrong type of value, expected {self._type.__name__}, got {value.__class__.__name__}')
+        self._value = value
+
+    @abstractmethod
+    def __call__(self, *args, **kwargs):
+        raise NotImplementedError
+
+    @abstractmethod
+    def clear(self):
+        raise NotImplementedError
+
+
+class StatisticInt(Statistic):
+    _value = 0
 
     def __call__(self):
-        self.value += 1
+        self._value += 1
 
     def clear(self):
-        self.value = 0
+        self._value = 0
+
+
+class StatisticList(Statistic):
+    _value = []
+
+    def __call__(self, value):
+        self._value.append(value)
+
+    def clear(self):
+        self._value = []
 
 
 @dataclass(slots=True)
 class BotRegularStats:
     callback_queries_handled = StatisticInt()
     inline_queries_handled = StatisticInt()
-    unique_users_served = StatisticInt()
+    unique_users_served = StatisticList()
     exceptions_caught = StatisticInt()
 
     def clear(self):
