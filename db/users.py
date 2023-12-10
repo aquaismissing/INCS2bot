388b18aa8f51ae8725c734523710077482352242
SyberiaK <syberiakey@gmail.com>
2023-12-10 21:35:46 +0500

    Added "last_bot_pm_id" for session sync

--- db/users.py
+++ db/users.py
@@ -1,4 +1,4 @@
-import sqlalchemy
+import sqlalchemy as sa
 from sqlalchemy_serializer import SerializerMixin
 
 from .db_session import SqlAlchemyBase
@@ -7,11 +7,12 @@ from .db_session import SqlAlchemyBase
 class User(SqlAlchemyBase, SerializerMixin):
     __tablename__ = 'users'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    userid = sqlalchemy.Column(sqlalchemy.Integer, unique=True)
-    current_menu_id = sqlalchemy.Column(sqlalchemy.String)
-    previous_menu_id = sqlalchemy.Column(sqlalchemy.String)
-    language = sqlalchemy.Column(sqlalchemy.String)
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    userid = sa.Column(sa.Integer, unique=True)
+    current_menu_id = sa.Column(sa.String)
+    previous_menu_id = sa.Column(sa.String)
+    language = sa.Column(sa.String)
+    last_bot_pm_id = sa.Column(sa.Integer)
 
     def __repr__(self):
         return f'<User(id={self.id}, userid={self.userid}, language={self.language})>'
