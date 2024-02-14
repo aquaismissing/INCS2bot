0d22a2b46a3b6ca63dbc1244bd6ec15605e9e541
SyberiaK <syberiakey@gmail.com>
2024-02-14 15:25:52 +0500

    Something got fixed in the lib but still bugs

--- collectors/game_coordinator.py
+++ collectors/game_coordinator.py
@@ -1,22 +1,13 @@
-# import gevent.monkey
-
-# gevent.monkey.patch_all()
-
-# import asyncio
+import asyncio
 import json
 import logging
 import platform
-# import sys
-# from threading import Thread
 import time
-
-# from csgo.client import CSGOClient
-# from steam.client import SteamClient
-# from steam.enums import EResult
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from steam import App
 from steam.ext.csgo import Client
+from steam.ext.csgo.protobufs.sdk import GcConnectionStatus
 
 
 if platform.system() == 'Linux':
@@ -30,23 +21,24 @@ import env
 import config
 from utypes import GameVersionData, States
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s | %(name)s: %(message)s',
+logging.basicConfig(format='%(asctime)s | %(name)s: %(message)s',
                     datefmt='%H:%M:%S — %d/%m/%Y',
                     force=True)
 
 logger = logging.getLogger('root')  # f'{config.BOT_NAME}.GCCollector'
+logging.getLogger('steam').setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 
 class GCCollector(Client):
-    APPS_TO_FETCH = App(id=730), App(id=740), App(id=741), App(id=745), App(id=2275500), App(id=2275530)
+    APPS_TO_FETCH = App(id=730), App(id=2275500), App(id=2275530)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
         self.scheduler = AsyncIOScheduler()
         self.scheduler.add_job(self.update_depots, 'interval', seconds=45)
-        self.scheduler.add_job(self.update_players_count, 'interval', seconds=45)
+        # self.scheduler.add_job(self.update_players_count, 'interval', seconds=45)
 
     async def login(self, *args, **kwargs):
         logger.info('Logging in...')
@@ -67,17 +59,16 @@ class GCCollector(Client):
         if result:
             self.scheduler.resume()
 
-    async def on_gc_connect(self):
+    async def on_gc_ready(self):
         logger.info('CS launched.')
         self.scheduler.start()
 
-    async def on_gc_status_change(self, message):
-        status = await message
-        logger.info(f'{status!r}')
+    async def on_gc_status_change(self, status: GcConnectionStatus):
+        logger.info(f'{status.name!r} (on_gc_status_change)')
 
-        # statuses = {0: States.NORMAL, 1: States.INTERNAL_SERVER_ERROR, 2: States.OFFLINE,
-        #             3: States.RELOADING, 4: States.INTERNAL_STEAM_ERROR}
-        # game_coordinator = statuses.get(status, States.UNKNOWN)
+        statuses = {0: States.NORMAL, 1: States.INTERNAL_SERVER_ERROR, 2: States.OFFLINE,
+                    3: States.RELOADING, 4: States.INTERNAL_STEAM_ERROR}
+        game_coordinator = statuses.get(status.value, States.UNKNOWN)
         #
         # with open(config.CACHE_FILE_PATH, encoding='utf-8') as f:
         #     cache = json.load(f)
@@ -88,31 +79,31 @@ class GCCollector(Client):
         # with open(config.CACHE_FILE_PATH, 'w', encoding='utf-8') as f:
         #     json.dump(cache, f, indent=4)
         #
-        # logger.info(f'Successfully dumped game coordinator status: {game_coordinator.literal}')
+        logger.info(f'Successfully dumped game coordinator status: {game_coordinator.literal}')
 
     async def update_depots(self):
         try:
             data = await self.fetch_product_info(apps=self.APPS_TO_FETCH)
-            logging.info(data)
-            # main_data = data[730]
+            logging.info(await self.fetch_product_info(apps=[App(id=2275500)]))
 
-            # ds_build_id = int(data[740]['depots']['branches']['public']['buildid'])
-            # valve_ds_change_number = data[741]['_change_number']
-            # cs2_app_change_number = data[2275500]['_change_number']
-            # cs2_server_change_number = data[2275530]['_change_number']
-            # sdk_build_id = int(data[745]['depots']['branches']['public']['buildid'])
-            # dpr_build_id = int(main_data['depots']['branches']['dpr']['buildid'])
-            # dprp_build_id = int(main_data['depots']['branches']['dprp']['buildid'])
-            # public_build_id = int(main_data['depots']['branches']['public']['buildid'])
+            data = {int(app.id): app for app in data}
+            logging.info(data)
+            main_data = data[730]
+
+            public_build_id = main_data.public_branch.build_id
+            dpr_build_id = main_data.get_branch('dpr').build_id
+            dprp_build_id = main_data.get_branch('dprp').build_id
+
+            # currently can't track
+            # cs2_app_change_number = data[2275500].change_number
+            # cs2_server_change_number = data[2275530].change_number
         except Exception:
             logger.exception('Caught an exception while trying to fetch depots!')
+            return
 
         # with open(config.CACHE_FILE_PATH, encoding='utf-8') as f:
         #     cache = json.load(f)
         #
-        # cache['sdk_build_id'] = sdk_build_id
-        # cache['ds_build_id'] = ds_build_id
-        # cache['valve_ds_changenumber'] = valve_ds_change_number
         # cache['cs2_app_changenumber'] = cs2_app_change_number
         # cache['cs2_server_changenumber'] = cs2_server_change_number
         # cache['dprp_build_id'] = dprp_build_id
@@ -120,16 +111,15 @@ class GCCollector(Client):
         #
         # if public_build_id != cache.get('public_build_id'):
         #     cache['public_build_id'] = public_build_id
-        #     Thread(target=self.update_game_version).start()
+        #     task = asyncio.create_task(some_coro(param=i))
         #
         # with open(config.CACHE_FILE_PATH, 'w', encoding='utf-8') as f:
         #     json.dump(cache, f, indent=4)
 
         logger.info('Successfully dumped game build IDs.')
 
-    @staticmethod
-    def update_game_version():
-        timeout = 1800
+    async def update_game_version(self):
+        timeout = 30 * 60
         timeout_start = time.time()
         while time.time() < timeout_start + timeout:
             try:
@@ -143,16 +133,20 @@ class GCCollector(Client):
                     for key, value in data.asdict().items():
                         cache[key] = value
 
-                with open(config.CACHE_FILE_PATH, 'w', encoding='utf-8') as f:
-                    json.dump(cache, f, indent=4)
+                    with open(config.CACHE_FILE_PATH, 'w', encoding='utf-8') as f:
+                        json.dump(cache, f, indent=4)
+                    return
             except Exception:
                 logging.exception('Caught an exception while trying to get new version!')
-                time.sleep(45)
-                continue
-            time.sleep(45)
+            await asyncio.sleep(45)
+
+        # sometimes steamdb updates the info much later (xPaw: Zzz...)
+        # because of this, we retry in an hour
+        await asyncio.sleep(60 * 60)
+        await self.update_game_version()
 
     async def update_players_count(self):
-        value = await self.get_app(730).player_count()
+        value = await self.get_app(730).player_count()  # freezes function entirely
 
         with open(config.CACHE_FILE_PATH, 'r', encoding='utf-8') as f:
             cache = json.load(f)
@@ -165,31 +159,22 @@ class GCCollector(Client):
         logger.info(f'Successfully dumped player count: {value}')
 
 
-# client = SteamClient()
-# client.set_credential_location(str(config.STEAM_CREDS_PATH))
-# cs = CSGOClient(client)
-
-
 # @client.on("error")
 # def handle_error(result):
 #     logging.info(f'Logon result: {result!r}')
-
 
 # @client.on('channel_secured')
 # def send_login():
 #     if client.relogin_available:
 #         client.relogin()
 
-
 # @client.on('connected')
 # def handle_connected():
 #     logging.info(f'Connected to {client.current_server_addr}')
 
-
 # @client.on('reconnect')
 # def handle_reconnect(delay):
 #     logging.info(f'Reconnect in {delay}s...')
-
 
 # @client.on('disconnected')
 # def handle_disconnect():
@@ -203,7 +188,6 @@ class GCCollector(Client):
 #     if result:
 #         logging.info(f'{client.logged_on=}')
 #         cs.emit('reload')
-
 
 # @cs.on('connection_status')
 # def gc_ready(status):
@@ -222,23 +206,16 @@ class GCCollector(Client):
 #
 #     logging.info(f'Successfully dumped game coordinator status: {game_coordinator.literal}')
 
-
-# @cs.on('ready')
-# def cs_launched():
-#     logging.info(f'CS launched.')
-
 # @cs.on('reload')
 # def cs_exit():
 #     logging.info(f'Reloading CS...')
 #     cs.launch()
-
 
 # @client.on('logged_on')
 # def handle_after_logon():
 #     cs.launch()
 #     Thread(target=depots_prepare).start()
 #     Thread(target=online_players).start()
-
 
 # def depots_prepare():
 #     loop = asyncio.new_event_loop()
@@ -247,91 +224,6 @@ class GCCollector(Client):
 #     loop.run_until_complete(depots())
 #     loop.close()
 
-
-# async def depots():
-#     while True:
-#         try:
-#             data = client.get_product_info(apps=[740, 741, 2275500, 2275530, 745, 730],
-#                                            timeout=15)['apps']
-#             main_data = data[730]
-#
-#             ds_build_id = int(data[740]['depots']['branches']['public']['buildid'])
-#             valve_ds_change_number = data[741]['_change_number']
-#             cs2_app_change_number = data[2275500]['_change_number']
-#             cs2_server_change_number = data[2275530]['_change_number']
-#             sdk_build_id = int(data[745]['depots']['branches']['public']['buildid'])
-#             dpr_build_id = int(main_data['depots']['branches']['dpr']['buildid'])
-#             dprp_build_id = int(main_data['depots']['branches']['dprp']['buildid'])
-#             public_build_id = int(main_data['depots']['branches']['public']['buildid'])
-#         except Exception:
-#             logging.exception('Caught an exception while trying to fetch depots!')
-#             time.sleep(45)
-#             continue
-#
-#         with open(config.CACHE_FILE_PATH, encoding='utf-8') as f:
-#             cache = json.load(f)
-#
-#         cache['sdk_build_id'] = sdk_build_id
-#         cache['ds_build_id'] = ds_build_id
-#         cache['valve_ds_changenumber'] = valve_ds_change_number
-#         cache['cs2_app_changenumber'] = cs2_app_change_number
-#         cache['cs2_server_changenumber'] = cs2_server_change_number
-#         cache['dprp_build_id'] = dprp_build_id
-#         cache['dpr_build_id'] = dpr_build_id
-#
-#         if public_build_id != cache.get('public_build_id'):
-#             cache['public_build_id'] = public_build_id
-#             Thread(target=gv_updater).start()
-#
-#         with open(config.CACHE_FILE_PATH, 'w', encoding='utf-8') as f:
-#             json.dump(cache, f, indent=4)
-#
-#         logging.info('Successfully dumped game build IDs.')
-#         time.sleep(45)
-
-
-# def gv_updater():
-#     timeout = 1800
-#     timeout_start = time.time()
-#     while time.time() < timeout_start + timeout:
-#         try:
-#             data = GameVersionData.request()
-#
-#             with open(config.CACHE_FILE_PATH, encoding='utf-8') as f:
-#                 cache = json.load(f)
-#
-#             if data.csgo_client_version != cache.get('csgo_client_version') \
-#                     or data.cs2_client_version != cache.get('cs2_client_version'):
-#                 for key, value in data.asdict().items():
-#                     cache[key] = value
-#
-#             with open(config.CACHE_FILE_PATH, 'w', encoding='utf-8') as f:
-#                 json.dump(cache, f, indent=4)
-#         except Exception:
-#             logging.exception('Caught an exception while trying to get new version!')
-#             time.sleep(45)
-#             continue
-#         time.sleep(45)
-#     sys.exit()
-
-
-# def online_players():
-#     while True:
-#         value = client.get_player_count(730)
-#
-#         with open(config.CACHE_FILE_PATH, 'r', encoding='utf-8') as f:
-#             cache = json.load(f)
-#
-#         if value != cache.get('online_players'):
-#             cache['online_players'] = value
-#
-#         with open(config.CACHE_FILE_PATH, 'w', encoding='utf-8') as f:
-#             json.dump(cache, f, indent=4)
-#
-#         logging.info(f'Successfully dumped player count: {value}')
-#         time.sleep(45)
-
-
 # @client.on('new_login_key')
 # def lol():
 #     logging.info(f'{client.login_key=}')
@@ -339,7 +231,7 @@ class GCCollector(Client):
 
 def main():
     collector = GCCollector()
-    collector.run(config.STEAM_USERNAME, config.STEAM_PASS, debug=True)
+    collector.run(refresh_token=config.STEAM_REFRESH_TOKEN, debug=True)
 
 
 if __name__ == '__main__':
