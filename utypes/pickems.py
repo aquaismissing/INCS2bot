5a87c09458392fa94533e929ae4043a9d4f5641c
SyberiaK <syberiakey@gmail.com>
2024-02-19 23:19:50 +0500

    Probably works... I'm not sure

--- utypes/pickems.py
+++ utypes/pickems.py
@@ -4,13 +4,16 @@ import json
 from pathlib import Path
 from pprint import pprint
 import re
+from typing import NewType
 
 import requests
 
 import config
+from db.users import User
 
 GET_TOURNAMENT_LAYOUT_API = 'https://api.steampowered.com/ICSGOTournaments_730/GetTournamentLayout/v1'
-GET_TOURNAMENT_PREDICTIONS_API = 'https://api.steampowered.com/ICSGOTournaments_730/GetTournamentFantasyLineup/v1?key={}&event={}&steamid={}&steamidkey={}'
+GET_TOURNAMENT_PREDICTIONS_API = 'https://api.steampowered.com/ICSGOTournaments_730/GetTournamentFantasyLineup/v1'
+UPLOAD_TOURNAMENT_PREDICTIONS_API = 'https://api.steampowered.com/ICSGOTournaments_730/UploadTournamentPredictions/v1'
 
 HEADERS = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0) Gecko/20100101 Firefox/80.0"}
 
@@ -18,43 +21,36 @@ HEADERS = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0) Gecko
 USER_AUTH_KEY_PATTERN = re.compile(r'([A-Za-z0-9]){4}-([A-Za-z0-9]){5}-([A-Za-z0-9]){4}')
 
 
-class Event(IntEnum):  # the first two used as an example
+TeamPickID = NewType('TeamPickID', int)
+
+
+class Event(IntEnum):  # the first two used as examples
     IEM_RIO_2022 = 20
     BLAST_PARIS_2023 = 21
     PGL_COPENHAGEN_2024 = 22
 
 
 @dataclass(frozen=True, slots=True)
-class MajorLayoutGroupTeam:
-    pickid: int
-
-
-@dataclass(frozen=True, slots=True)
-class MajorLayoutGroupPick:
-    index: int
-    pickids: list[int]
-
-
-@dataclass(frozen=True, slots=True)
 class MajorLayoutGroup:
-    groupid: int
+    id: int
     name: str
     points_per_pick: int
     picks_allowed: bool
-    teams: list[MajorLayoutGroupTeam]
-    picks: list
+    teams: list[TeamPickID]
+    picks: list[list[TeamPickID]]
 
 
 @dataclass(frozen=True, slots=True)
 class MajorLayoutSection:
-    sectionid: int
+    id: int
     name: str
-    groups: list[MajorLayoutGroup]  # usually only one
+    groups: list[MajorLayoutGroup]  # only one for Challengers and Legends stages
+    # todo: maybe do specific dataclasses for stages instead of generic one?
 
 
 @dataclass(frozen=True, slots=True)
 class MajorLayoutTeam:
-    pickid: int
+    pickid: TeamPickID
     logo: str
     name: str
 
@@ -90,19 +86,26 @@ class MajorPickEmService:
         if not self.is_event_ongoing():
             return
 
-        layout = requests.get(GET_TOURNAMENT_LAYOUT_API,
-                              params={'key': self.steam_webapi_key, 'event': self.event.value},
-                              headers=HEADERS, timeout=15).json()['result']
+        response = requests.get(GET_TOURNAMENT_LAYOUT_API,
+                                params={'key': self.steam_webapi_key, 'event': self.event.value},
+                                headers=HEADERS, timeout=15)
+        response.raise_for_status()
 
+        layout = response.json()['result']
         event = Event(layout['event'])
         name = layout['name']
-        for i, section in enumerate(layout['sections']):
-            for j, group in enumerate(section['groups']):
-                for k, team in enumerate(group['teams']):
-                    group['teams'][k] = MajorLayoutGroupTeam(team['pickid'])
-                for k, pick in enumerate(group['picks']):
-                    group['picks'][k] = MajorLayoutGroupPick(**pick)
-                section['groups'][j] = MajorLayoutGroup(**group)
+        for section in layout['sections']:
+            for i, group in enumerate(section['groups']):
+                for j, team in enumerate(group['teams']):
+                    group['teams'][j] = TeamPickID(team['pickid'])
+
+                for pick in group['picks']:
+                    j = pick.pop('index')  # to ensure the order remains
+                    group['picks'][j] = list(pick.values())
+                group['id'] = group.pop('groupid')
+
+                section['groups'][i] = MajorLayoutGroup(**group)
+            section['id'] = section.pop('sectionid')
 
         challengers_stage = MajorLayoutSection(**layout['sections'][0])
         legends_stage = MajorLayoutSection(**layout['sections'][1])
@@ -123,11 +126,9 @@ class MajorPickEmService:
 
         for i, section in enumerate(layout['sections']):
             for j, group in enumerate(section['groups']):
-                for k, team in enumerate(group['teams']):
-                    pickid = team['pickid']
-                    group['teams'][k] = MajorLayoutGroupTeam(pickid)
-                for k, pick in enumerate(group['picks']):
-                    group['picks'][k] = MajorLayoutGroupPick(**pick)
+                for k, pickid in enumerate(group['teams']):
+                    group['teams'][k] = TeamPickID(pickid)
+
                 section['groups'][j] = MajorLayoutGroup(**group)
             layout['sections'][i] = MajorLayoutSection(**section)
 
@@ -136,34 +137,96 @@ class MajorPickEmService:
 
         return MajorLayout(**layout)
 
-
-@dataclass(frozen=True, slots=True)
-class UserMajorPickEm:
-    """User-specific Pick'Em information and methods."""
-
-    pickem_service: MajorPickEmService
-    steamid: int  # todo: replace with db.User
-    auth_key: str
-
-    def get_predictions(self):
-        if not self.pickem_service.is_event_ongoing():
+    def get_user_predictions(self, user: User):
+        if not self.is_event_ongoing():
             return
+
         picks = requests.get(GET_TOURNAMENT_PREDICTIONS_API,
-                             params={'key': self.pickem_service.steam_webapi_key,
-                                     'event': self.pickem_service.event.value,
-                                     'steamid': self.steamid,
-                                     'steamidkey': self.auth_key},
+                             params={'key': self.steam_webapi_key,
+                                     'event': self.event.value,
+                                     'steamid': user.steamid,
+                                     'steamidkey': user.pickem_auth_key},
                              headers=HEADERS, timeout=15).json()['result']['picks']
         return [MajorPredictionsPick(**pick) for pick in picks]
 
-    def set_challengers_stage_predictions(self, pickids: list[int]):
+    def set_challengers_stage_predictions(self, user: User, pickids: list[int]):
         if len(pickids) != 9:
             raise ValueError('a count of pickids must be equal to 9')
-        # more silly stuff there...
+
+        params = {'key': self.steam_webapi_key}
+        sectionid = 0
+        groupid = 0
+
+        data = {
+            'event': self.event.value, 'steamid': user.steamid, 'steamidkey': user.pickem_auth_key,
+            'sectionid': sectionid, 'groupid': groupid, 'pickid': pickids[0]
+        }
+        for i, pickid in enumerate(pickids[1:], 1):
+            data[f'sectionid{i}'] = sectionid
+            data[f'groupid{i}'] = groupid
+            data[f'pickid{i}'] = pickid
+
+        response = requests.post(UPLOAD_TOURNAMENT_PREDICTIONS_API, params=params, data=data)
+        response.raise_for_status()
+
+    def set_legends_stage_predictions(self, user: User, pickids: list[int]):
+        if len(pickids) != 9:
+            raise ValueError('a count of pickids must be equal to 9')
+
+        params = {'key': self.steam_webapi_key}
+        sectionid = 1
+        groupid = 0
+
+        data = {
+            'event': self.event.value, 'steamid': user.steamid, 'steamidkey': user.pickem_auth_key,
+            'sectionid': sectionid, 'groupid': groupid, 'pickid': pickids[0]
+        }
+        for i, pickid in enumerate(pickids[1:], 1):
+            data[f'sectionid{i}'] = sectionid
+            data[f'groupid{i}'] = groupid
+            data[f'pickid{i}'] = pickid
+
+        response = requests.post(UPLOAD_TOURNAMENT_PREDICTIONS_API, params=params, data=data)
+        response.raise_for_status()
+
+    def set_playoff_predictions(self, user: User, pickids: list[int]):
+        if len(pickids) != 7:
+            raise ValueError('a count of pickids must be equal to 7')
+
+        params = {'key': self.steam_webapi_key}
+
+        sectionid = 2
+        data = {
+            'event': self.event.value, 'steamid': user.steamid, 'steamidkey': user.pickem_auth_key,
+            'sectionid': sectionid, 'groupid': 0, 'pickid': pickids[0]
+        }
+        for i, pickid in enumerate(pickids[1:4], 1):
+            data[f'sectionid{i}'] = sectionid
+            data[f'groupid{i}'] = i
+            data[f'pickid{i}'] = pickid
+
+        sectionid = 3
+        for i, pickid in enumerate(pickids[4:6], 4):
+            data[f'sectionid{i}'] = sectionid
+            data[f'groupid{i}'] = i - 4
+            data[f'pickid{i}'] = pickid
+
+        sectionid = 4
+        data['sectionid6'] = sectionid
+        data['groupid6'] = 0
+        data['pickid6'] = pickids[6]
+
+        response = requests.post(UPLOAD_TOURNAMENT_PREDICTIONS_API, params=params, data=data)
+        response.raise_for_status()
 
 
-a = MajorPickEmService(config.STEAM_API_KEY, Event.BLAST_PARIS_2023)
-data = a.get_event_layout()
-pprint(data)
-print(data.event)
-print(type(data.event))
+def main():
+    a = MajorPickEmService(config.STEAM_API_KEY, Event.BLAST_PARIS_2023)
+    layout = a.get_event_layout()
+    pprint(layout)
+    print(layout.event)
+    print(type(layout.event))
+
+
+if __name__ == '__main__':
+    main()
