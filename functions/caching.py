6bebec0dde44a228e084fc17b58f670821bbc8d9
SyberiaK <syberiakey@gmail.com>
2025-04-26 01:36:22 +0500

    Implement safe caching via filelock

--- functions/caching.py
+++ functions/caching.py
@@ -1,19 +1,26 @@
 import json
 from pathlib import Path
 
+import filelock
 
 __all__ = ['load_cache', 'dump_cache', 'dump_cache_changes']
 
 
-def load_cache(path: Path) -> dict[str, ...]:
-    with open(path, 'r', encoding='utf-8') as f:
-        return json.load(f)
+def get_filelock(path: Path, *, timeout: int = 10):
+    return filelock.FileLock(path.with_suffix(path.suffix + '.lock'), timeout=timeout)
 
 
-def dump_cache(path: Path, cache: dict[str, ...]):
-    with open(path, 'w', encoding='utf-8') as f:
-        json.dump(cache, f, indent=4, ensure_ascii=False)
+def load_cache(path: Path, *, timeout: int = 10) -> dict[str, ...]:
+    with get_filelock(path, timeout=timeout):
+        with open(path, 'r', encoding='utf-8') as f:
+            return json.load(f)
 
 
-def dump_cache_changes(path: Path, changes: dict[str, ...]):
-    dump_cache(path, load_cache(path) | changes)
+def dump_cache(path: Path, cache: dict[str, ...], *, timeout: int = 5):
+    with get_filelock(path, timeout=timeout):
+        with open(path, 'w', encoding='utf-8') as f:
+            json.dump(cache, f, indent=4, ensure_ascii=False)
+
+
+def dump_cache_changes(path: Path, changes: dict[str, ...], *, timeout: int = 10):
+    dump_cache(path, load_cache(path, timeout=timeout) | changes, timeout=timeout)
